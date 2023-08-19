97f1a64c144e154a54552b0685b87d3ceb5935ee
averybrubeck <averybrubeck@yahoo.com>
2023-08-19 13:14:45 -0400

    created initial array

--- game.js
+++ game.js
@@ -1,1 +1,16 @@
-console.log('Hello world')
+//initiate the game/start button or message
+//need an array for the rock paper siccors 
+//need to be able to compare the three
+// need the computer to randomly choose between
+//need player input 
+//game loop/copy code 5 times 
+//need to keep score1
+//need ending messages winning and losing
+
+
+const rock = 'rock';
+const paper = 'paper';
+const scissors = 'scissors';
+const choices = [rock, paper, scissors];
+
+console.log(choices);
