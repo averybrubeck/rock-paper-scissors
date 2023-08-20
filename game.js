bec9e1a111e83a251b7e48c583545bb7f1829eb5
averybrubeck <averybrubeck@yahoo.com>
2023-08-20 11:51:33 -0400

    removed peusdocode

--- game.js
+++ game.js
@@ -1,14 +1,5 @@
 
 
-//initiate the game/start button or message
-//need an array for the rock paper siccors 
-//need to be able to compare the three
-// need the computer to randomly choose between
-//need player input 
-//game loop/copy code 5 times 
-//need to keep score1
-//need ending messages winning and losing
-
 
 const rock = 'rock';
 const paper = 'paper';
