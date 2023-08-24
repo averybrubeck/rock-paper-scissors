e110049d8fe93d36aa4cc4b250687e6f852e2d83
averybrubeck <averybrubeck@Yahoo.com>
2023-08-24 13:02:50 -0400

    added compareChoises function

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ const rock = 'rock';
 const paper = 'paper';
 const scissors = 'scissors';
 const choices = [rock, paper, scissors];
-let playerChoice = choices;
+let playerChoice = null;
 const rockIcon = document.querySelector('.rock');
 const paperIcon = document.querySelector('.paper');
 const sciIcon = document.querySelector('.scissors');
@@ -13,6 +13,7 @@ let gameCount = document.querySelector('.gameCount');
 let computerChoice = choices[Math.floor((Math.random() * 3))];
 
 playerChoose();
+compareChoices();
 
 
 function playerChoose() {
@@ -20,19 +21,32 @@ rockIcon.addEventListener('click', function(){
         playerChoice = rock;
         console.log(rockIcon);    
 });
-
 paperIcon.addEventListener('click', function(){ 
     playerChoice = paper;
     console.log(paperIcon);
     paperIcon.style.transform = "scale(1.2)";
     paperIcon.style.transition = "transform 0.25 ease";
 });
-
 sciIcon.addEventListener('click', function(){
     playerChoice = scissors;
     console.log(sciIcon);  
 })
+};
 
+
+function compareChoices() {
+    if (playerChoice === computerChoice) {
+        alert('Its a tie!');
+    } else if (
+        (playerChoice === 'rock' && computerChoice === 'scissors') ||
+        (playerChoice === 'paper' && computerChoice === 'rock') ||
+        (playerChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+        winCount++;
+    } else {
+        loseCount++;
+    }
+    gameCount++;
 };
 
 
@@ -71,49 +85,6 @@ sciIcon.addEventListener('click', function(){
 
 
 
-
-
-
-/*checks for 5 games played along with winners and looser
-function gameCheck(){
-    if (gameCount === 5 && winCount > looseCount) {
-        alert('You"re a super winner');
-    }else if (gameCount === 5 && looseCount > winCount) {
-        alert('the PC has taken over');
-    }
-    else;
-}
-
-*/
-//riund played, ends when user or pc wins a round
-/*function round() {
-if (computerChoice() === rock && playerChoice === paper) {
-    alert('winner');
-    winCount++;
-    gameCount++;
-} else if(computerChoice() === scissors && playerChoice === rock) {
-    alert('winner');
-    winCount++;
-    gameCount++;
-} else if(computerChoice() === paper && playerChoice === scissors) {  
-    alert('winner');
-    winCount++;
-    gameCount++;
-} else(
-    loseCount++, gameCount++ && alert('The PC Won this time')
-)
-console.log(computerChoice());
-}
-
-
-
-
-round();
-round();
-
-
-
-*/
 
 
 
